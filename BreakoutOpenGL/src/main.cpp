b8ee58abb9038db2cd5a3b0de14ec117a2039589
Julian <j.pearbach@berkeley.edu>
2022-12-06 18:00:54 -0800

    Almost have glew initializing successfully.

--- BreakoutOpenGL/src/main.cpp
+++ BreakoutOpenGL/src/main.cpp
@@ -1,10 +1,37 @@
+/*
+	Welcome to my Breakout clone!
+*/
+
+// SDL2 - Windowing, Input
 #include "SDL.h"
+// glew - OpenGL function fetching 
+#include <GL/glew.h>
 #include <iostream>
 
 int main(int argc, char* argv[]) 
 {
 	// Init SDL!
 	SDL_Init(SDL_INIT_EVERYTHING);
+
+	// The window... into your soul.
+	SDL_Window* window;
+	window = SDL_CreateWindow("Breakout!",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		600, 600,
+		SDL_WINDOW_SHOWN);
+
+	// Context is key.
+	SDL_GLContext SDL_GL_CreateContext(window);
+		
+
+	GLenum result = glewInit();
+	if (result != GLEW_OK)
+	{
+		std::cout << "Error! glewInit() != GLEW_OK ... glew failed to initialize? : " << result << std::endl;
+	}
+	
+	//std::cout << std::endl << glGetString(GL_VERSION) << std::endl;
 
 	std::cout << "Hello world!" << std::endl;
 	return 0;
